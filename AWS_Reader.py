312921035733216b37c75610b290f7fe8e94c910
James (Jimmy) Reed <117855743+JCReed26@users.noreply.github.com>
2025-01-08 13:55:57 -0500

    ok ok ok. I downloaded the AWS CLI on my device. Made and instance on AWS but turned it off. I created an S3 bucket and from the management console added 2 images. When I run it the program returns information. So lets go

--- AWS_Reader.py
+++ AWS_Reader.py
@@ -1,31 +1,10 @@
-
 import boto3
-
-#this is from a 1135page developer guide for Rekognition
-#page 666 is the python code below
-def detect_text(bucket, photo):
-
-    session = boto3.Session(profile_name='default')
-    client = session.client('rekognition')
-
-    response = client.detect_text(Image={'S3Object': {'Bucket': bucket, 'Name': photo}})
-
-    textDetections = response['TextDetections']
-    print('Detected text:' + text['DetectedText'])
-    print(f"Confidence: {text['Confidence']}")
-    print(f"Id: {text['Id']}")
-    if 'ParentId' in text:
-        print(f"Parent Id: {text['ParentId']}")
-    print(f"Type: {text['Type']}")
-    print()
-
-    return len(textDetections)
 
 #this is the github version
 if __name__ == "__main__":
 
-    bucket = 'bucketname'
-    photo = 'photo.jpg'
+    bucket = 'jims-receipt-bucket'
+    photo = 'IMG_3138.jpg'
 
     client = boto3.client('rekognition')
 

96cdcdc9397b47812df000bd477ad91032d17323
JCReed26 <117855743+JCReed26@users.noreply.github.com>
2025-02-12 12:33:58 -0500

    it is working and compiling need to actually test it

--- Receipt_Reader.py
+++ Receipt_Reader.py
@@ -1,29 +1,50 @@
-import cv2
 from PIL import Image
 import pytesseract
 import openai
 import os 
 from dotenv import load_dotenv
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import io
 
+#get env api key
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def get_image():
-    return "IMG_3144.jpg"
 
-def process_image_with_tesseract(image_path):
-    image = Image.open(image_path)
-    if image is None:
-        print(f"Error: Unable to read image at {image_path}")
-        return
-    image = image.convert('L')
+#create running app 
+app = FastAPI()
 
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    text = pytesseract.image_to_string(image)
-    lines = text.split("\n")
-    json_data = extract_ai_data(lines)
-    return json_data
+#async allow func to wait for image
+async def process_image_with_tesseract(image_data):
+    try:
+        # converts to bytes then to image 
+        image = Image.open(io.BytesIO(image_data))
+        image = image.convert("L") # makes it easier to differentiate between black and white
+
+
+        #local tesseract-ocr WILL NOT WORK WITHOUT
+        # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+        #docker tesseract-ocr WILL NOT WORK WITHOUT
+        pytessseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract' #the linux path in docker
+
+        #process image to output
+        text = pytesseract.image_to_string(image)
+        lines = text.split("\n")
+        json_data = extract_ai_data(lines)
+        return json_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 def extract_ai_data(lines):
     response = openai.ChatCompletion.create(
@@ -48,11 +69,10 @@ def extract_ai_data(lines):
     json_data = response['choices'][0]['message']['content']
     return json_data
 
-def main():
-    image_path = get_image()
-    json_data = process_image_with_tesseract(image_path)
-    # Print the JSON data
-    print(json_data)
+@app.post("/process_image") #POST request ... is the right way? but why?
+async def process_image(file: UploadFile = File(...)): 
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="Must upload a photo")
 
-if __name__ == '__main__':
-    main()
+    contents = await file.read()
+    return await process_image_with_tesseract(contents)
